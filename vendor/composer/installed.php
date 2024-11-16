ab1db8483e86fc65fbe4f90e063fa08214570b30
Md. Aminur Islam <eng.aminurislam@gmail.com>
2024-11-16 11:40:05 +0600

    Added missing vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'welabs/metadata-viewer',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '5eedff54cb40085f8ace9afa1d71b4cb98baa3d2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'welabs/metadata-viewer' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '5eedff54cb40085f8ace9afa1d71b4cb98baa3d2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
